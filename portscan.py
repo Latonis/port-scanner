47a511939d595ab53ab6c6bc24d8ffa1466f53a5
Jacob Latonis <latonis@wisc.edu>
2020-05-08 20:50:11 -0700

    better readability

--- portscan.py
+++ portscan.py
@@ -1,18 +1,20 @@
 import socket
 
-target = "google.com"
-sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sockt.settimeout(0.5)
+target = "localhost"
+targetIP = socket.gethostbyname(target)
 ports = []
 
 
 def port_scan(port_num):
+    sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sockt.settimeout(0.1)
     if sockt.connect_ex((target, port_num)) == 0:
         ports.append(port_num)
     sockt.close()
 
 
-for x in range(1024):
+for x in range(1,1024):
     port_scan(x)
 
-print(ports)
+for port in ports:
+    print("Port {}: OPEN".format(port))
